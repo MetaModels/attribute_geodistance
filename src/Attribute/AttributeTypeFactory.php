8ee680b9ecd58927a4b9f75f7296c7b7ab6f0289
zonky2 <info@e-spin.de>
2021-01-18 18:51:26 +0100

    Change icon

--- src/Attribute/AttributeTypeFactory.php
+++ src/Attribute/AttributeTypeFactory.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_geodistance.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,7 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -48,7 +48,7 @@ class AttributeTypeFactory extends AbstractSimpleAttributeTypeFactory
         $this->input = $input;
 
         $this->typeName  = 'geodistance';
-        $this->typeIcon  = 'bundles/metamodelsattributegeodistance/image/numeric.png';
+        $this->typeIcon  = 'bundles/metamodelsattributegeodistance/image/geodistance.png';
         $this->typeClass = GeoDistance::class;
     }
 
