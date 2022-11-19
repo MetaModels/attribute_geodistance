5a3dffddbfdf3d8dba30c15da33cf6c78c5b7a0a
Sven Baumann <baumann.sv@gmail.com>
2022-11-19 08:06:07 +0100

    Fixed author validation issue

--- src/Migration/AddCountryMigration.php
+++ src/Migration/AddCountryMigration.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_geodistance.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels/attribute_geodistance
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
